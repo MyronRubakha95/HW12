6077117b38cc16f804cf84942c55134dabb3187a
MyronRubakha95 <myron11r@gmail.com>
2022-02-06 11:47:45 +0200

    new12

--- IdeaProjects/HW12/src/com/company/Main.java
+++ IdeaProjects/HW12/src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,19 +8,12 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String args0 = "D:/Cursor/temp1.txt";
         String args1 = "D:/temp1.txt";
 
 
-        File file = new File(args1);
-        file.createNewFile();
-        FileReader inputStream = null;
-        FileWriter outputStream = null;
-
-        try {
-            inputStream = new FileReader(args0);
-            outputStream = new FileWriter(args1);
+        try (FileReader inputStream = new FileReader(args0); FileWriter outputStream = new FileWriter(args1)) {
 
             int a;
             while ((a = inputStream.read()) != -1) {
@@ -32,9 +24,6 @@ public class Main {
         } catch (IOException e) {
             System.err.println(e.getMessage());
 
-        } finally {
-            inputStream.close();
-            outputStream.close();
         }
     }
 }
